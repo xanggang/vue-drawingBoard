d37a0359a358171e945f4213ce755d5d5f3bf875
lynn <1057516363@qq.com>
2019-07-16 17:15:35 +0800

    update

--- packages/canvas/LayerManagement.vue
+++ packages/canvas/LayerManagement.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="container">
+  <div class="container" @mouseup="handleMouseup" ref="container">
     <div class="img-bar"
          v-for="(layer, index) in layerList"
          :key="index"
          @mousedown="handleMousedown"
          @mousemove="handleMousemove"
-         @mouseup="handleMouseup"
     >
       <img class="preview-img"
            width="50"
@@ -24,27 +23,44 @@
   @Component({})
   export default class LayerManagementClass extends Vue {
     @Prop() layerList!: LayerClass[]
+    $refs!:{
+      container: HTMLElement
+    }
 
     painting: boolean = false
 
     currentImgElement!: HTMLElement | null
 
+    // 容器下边线
+    containerPositionBottom: number = 0;
+    // 容器上边线
+    containerPositionTop: number = 0;
+
+    currentImgElementPositionTop: number = 0;
+
+
+
     handleMousedown(e: any) {
-      this.currentImgElement = e.target
-      console.log(e);
-      console.log(e);
       this.painting = true
+      this.currentImgElement = e.target
+      this.currentImgElementPositionTop = this.currentImgElement!.offsetTop
+      const container = this.$refs.container
+      this.containerPositionBottom = container.offsetTop + container.offsetHeight
+      this.containerPositionTop = container.offsetTop
     }
 
     handleMousemove(e: MouseEvent) {
       if (!this.painting) return
-      if (! this.currentImgElement.style) return
-      this.currentImgElement.style.transform = `translate(${e.x}px, ${e.y}px)`
+      if (!this.currentImgElement!.style) return
+      this.currentImgElement!.style.transform = `translateY(${e.clientY - this.currentImgElementPositionTop}px)`
     }
 
     handleMouseup() {
+      this.currentImgElement!.style.transform = `translateY(0px)`
       this.currentImgElement = null
-
+      this.containerPositionTop = 0
+      this.currentImgElementPositionTop = 0
+      this.painting = false
     }
   }
 </script>
@@ -53,11 +69,11 @@
   .container {
     position: absolute;
     left: 520px;
-    top: 20px;
+    top: 10px;
     z-index: 1001;
     height: 504px;
     overflow: auto;
-    transform: translate(20px, 20px);
+    border: 1px solid red;
     .img-bar {
       width: 50px;
       height: 50px;
